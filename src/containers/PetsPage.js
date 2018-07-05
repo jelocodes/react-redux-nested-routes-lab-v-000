e71bb2971605131bc67f4d64e46dff788e2fc4f4
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-07-04 20:17:20 -0400

    Done.

--- src/containers/PetsPage.js
+++ src/containers/PetsPage.js
@@ -6,20 +6,19 @@ import PetsNew from './PetsNew';
 import PetsShow from './PetsShow';
 import PetsList from '../components/PetsList';
 
-class PetsPage extends Component {
-
-  componentDidMount() {
-    this.props.fetchPets();
-  }
-
-  render() {
-    return (
-      <div>Pets Page</div>
-    )
-  }
-};
-
-const mapStateToProps = state => {
+const PetsPage = ({ match, pets }) => 
+  <div>
+    <PetsList pets={pets} />
+    <Switch> 
+      <Route path={`${match.url}/new`} component={PetsNew} />
+      <Route path={`${match.url}/:petId`} component={PetsShow}/>
+      <Route exact path={match.url} render={() => (
+        <h3>Please select a Pet from the list.</h3>
+      )}/>
+    </Switch>
+  </div>;
+ 
+const mapStateToProps = (state) => {
   return {
     pets: state.pets
   };
